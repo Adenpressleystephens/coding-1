c64ff76b3c2f1f7c5972c3f2ab7a17bbe9e43952
Adenpressleystephens <apressley-stephens894@boyslatin.net>
2026-09-01 14:24:35 +0000

    DATA TYPE NOTES

---
+++ datatypes.py
@@ -0,0 +1,27 @@
+#strings-characters that are wrapped withon quotes
+#characters can be letters,numbers,and symbols
+# you can use both singal and double quotations
+# marks for strings
+
+  "Aden Pressley-Stephens"
+
+   "123 main street
+   
+   "this cost $100 dollares"
+
+   #the hastage is a marker that makes anything writen
+   # behing it invisable to ht ecomputar
+
+   # integers- datatype that are just whole numbers,or numbers
+   # WITHOUT A DECIMAL POINT. WE DONT HAVE TO ADD ANY SPECIAL SYMBOLS
+
+   10
+   20
+  200000
+
+  #BOLLEAN- A DATA TYPE THAT CAN ONLY BE EITHER TRUE OR FALSE
+  # WE DO NOT USE QUOTATION MARKERS FOR BOOLEAN VALUES. THE COMPUTER
+  # UNDERSTAND THESE WORDS AS SOMTHING UNIQUE
+
+  FALSE
+  TRUE
