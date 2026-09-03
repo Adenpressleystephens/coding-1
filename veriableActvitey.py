3028fa91d154e3e0f62b33e5dc18fa984b5ac83c
Adenpressleystephens <apressley-stephens894@boyslatin.net>
2026-09-03 14:20:48 +0000

    VARIABLE WORK

---
+++ veriableActvitey.py
@@ -0,0 +1,22 @@
+# Variable-a container that stores sata.you can
+# Names your contanier almost anything you want,so long
+# As it is descriptive
+
+name="aden pressley stephens
+Yearsoftexp=0
+gradute=True"
+
+# variable naming rules
+# 1. A variable musat start with letter and cannot statr with a number
+# 2. A variable can be mixed of letters and numbers but not symbols
+#3. A variable cannot be a python keyword
+
+APPLECOUNT=85
+STUDENTCOUNT=50
+INSURANCECOST=TRUE
+SILVERGYM=FALUSE
+GOLDGYM=TRUE
+BRONZEGYM=FAULSE
+SPRITEDRINK=3.79
+COCOLADRINK=4.67
+FANTADRINK=5.34
