f1e1b6887d43ea08aa15bdfc6f63927a148bfe69
Adenpressleystephens <apressley-stephens894@boyslatin.net>
2026-09-09 14:37:35 +0000

    operators py

---
+++ operators.py
@@ -0,0 +1,22 @@
+# we used a singnal equal sighn to represent the assinment
+# operatot
+
+name = Aden
+grade = 10
+school = true
+
+# arthmetic operators- used on numerical.
+# data types to perform calulations.
+# intergers (whole numbers) and floats (decimal numbers)
+
+print(3 + 3)# addition operator
+print(3.9 - 3)# subtraction operator
+print(12 / 3)#divison operator
+print(3 x 4)#multiplication operator
+
+#comperison operators - set of symbols used
+# to assess if data is the same or differnt and
+# how they differ
+
+# 2 equal signs compare if somthing is the same
+Print("aden"=="aden) #same as (false)
