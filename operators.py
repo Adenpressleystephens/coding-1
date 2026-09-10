c4b7ff6874b22981181b8b06f7c0ca18106fe1e0
Adenpressleystephens <apressley-stephens894@boyslatin.net>
2026-09-10 14:33:53 +0000

    operator day 2

--- operators.py
+++ operators.py
@@ -9,14 +9,39 @@ school = true
 # data types to perform calulations.
 # intergers (whole numbers) and floats (decimal numbers)
 
-print(3 + 3)# addition operator
-print(3.9 - 3)# subtraction operator
-print(12 / 3)#divison operator
-print(3 x 4)#multiplication operator
+print(3 + 3) # addition operator
+print(3.9 - 3 )# subtraction operator
+print(12 / 3) # divison operator
+print(3 x 4) # multiplication operator
 
 #comperison operators - set of symbols used
 # to assess if data is the same or differnt and
 # how they differ
 
 # 2 equal signs compare if somthing is the same
-Print("aden"=="aden) #same as (false)
+Print("aden"=="aden") #same as (false)
+PRINT(" 2" == 2) # SAME AS (FALSE)
+PRINT(2.0 == 2) # SAME AS (TRUE)
+
+# NOT EQUAL IS WRITTEN WITH !=
+# THIS IS TO CHECK AND FILTER FOR VALUSE THAT ARE NOT
+# THE SAME
+PRINT(200 != 100) # TRUE- THESE ARE NOT THE SAME
+PRINT(300 != 300) # FALSE- THESE ARE THE SAME
+
+
+
+# LOGICAL OPERATORS- COMPARES 2 CONDITION TO CHECK IF
+# THEY ARE TRUE OR FALSE
+
+# AND - CHECK IF 2 CONDITIONS ARE TRUE. IF YES, THE FINAL
+# result is true
+print(3 > 1 and 100 >50) # this woulde come out to be true
+
+#or- check if only 1 conditions is true. if yes,
+# the final result will be true
+print(3 < 1 or 100 > 50)
+
+# not - the "opposite day" operator. it will reverse the
+# result of the logical operators
+print(not(3 > 1 and 100 >50))
