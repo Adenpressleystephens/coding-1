7f88572ecdd784217a04deb1a301a73be86d0c14
Adenpressleystephens <apressley-stephens894@boyslatin.net>
2026-09-16 14:33:50 +0000

    scenario code

--- operatorsActivity.py
+++ operatorsActivity.py
@@ -6,3 +6,18 @@ PRINT("coding 1" == "Coding 1")
 # False
 print("0" != 0) 
 # True
+
+print(10.99 + 399.99)
+# True
+
+print(98.87 + 100.00)
+# False
+
+Print(200.00 + 0.07)
+#True
+
+print(Gpa + appProgram)
+# Ture
+
+print(parent contact1 > parent contact2)
+# False
